514473b9365faab14305c45e2e5674e379a871f6
Omar Branez <omar.branez@gmail.com>
2021-02-06 23:56:14 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+    if number <= 1
+        return false
+    end
+    (2..(number-1)).each do |divisor|
+        if number % divisor == 0
+            return false            
+        end
+    end
+    return true
+end
