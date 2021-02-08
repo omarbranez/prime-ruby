aea79cf0ac1c8ce7fd982f93fa53c0bb72650e71
Omar Branez <omar.branez@gmail.com>
2021-02-08 00:57:54 -0500

    commit tree 2820dc3a63b49c3e070cd799469756c0244d83bf

--- prime.rb
+++ prime.rb
@@ -1,10 +1,11 @@
 # Add  code here!
-def prime?(number)
-    if number <= 1
+def prime?(number)                      
+    if number <= 1      #negatives, 0, and 1 are not prime numbers
         return false
     end
-    (2..(number-1)).each do |divisor|
-        if number % divisor == 0
+    (2..(number-1)).each do |divisor| 
+    #created a range where 2 is the smallest prime and the number one less than the test subject.
+        if number % divisor == 0   #if the number is divisible, aka it doesn't leave a remainder, it is not prime
             return false            
         end
     end
